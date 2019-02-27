9ea8cddf6812915869f18109079efccc5c2b157f
GengFei <31059500+cybertoken@users.noreply.github.com>
2019-02-27 14:25:29 +0800

    Update index.js
    支持多个数字同时运算

--- index.js
+++ index.js
@@ -1,25 +1,51 @@
-var calcPos = num => num.toString().indexOf(".") > -1 ? num.toString().split(".")[1].length : 0;
-var parseNum = num => parseInt(num.toString().replace(".", ""));
+let calcPos = num =>
+  num.toString().indexOf(".") > -1 ? num.toString().split(".")[1].length : 0;
+let parseNum = num => parseInt(num.toString().replace(".", ""));
 
-export default {
+let FCU = {
   // 加
-  add: (n1, n2) => {
-    var p1 = calcPos(n1), p2 = calcPos(n2);
-    var m = Math.pow(10, Math.max(p1, p2));
+  _add: (n1, n2) => {
+    let p1 = calcPos(n1),
+      p2 = calcPos(n2);
+    let m = Math.pow(10, Math.max(p1, p2));
     return (n1 * m + n2 * m) / m;
   },
+  add: (...nums) => {
+    let result = nums[0];
+    for (let i = 1; i < nums.length; i++) result = FCU._add(result, nums[i]);
+    return result;
+  },
   // 减
-  sub: (n1, n2) => FCU.add(n1, -n2),
+  _sub: (n1, n2) => FCU.add(n1, -n2),
+  sub: (...nums) => {
+    let result = nums[0];
+    for (let i = 1; i < nums.length; i++) result = FCU._sub(result, nums[i]);
+    return result;
+  },
   // 乘
-  mul: (n1, n2) => {
-    var p1 = calcPos(n1), p2 = calcPos(n2);
-    var m = Math.pow(10, p1 + p2);
-    return parseNum(n1) * parseNum(n2) / m;
+  _mul: (n1, n2) => {
+    let p1 = calcPos(n1),
+      p2 = calcPos(n2);
+    let m = Math.pow(10, p1 + p2);
+    return (parseNum(n1) * parseNum(n2)) / m;
+  },
+  mul: (...nums) => {
+    let result = nums[0];
+    for (let i = 1; i < nums.length; i++) result = FCU._mul(result, nums[i]);
+    return result;
   },
   // 除
-  div: (n1, n2) => {
-    var p1 = calcPos(n1), p2 = calcPos(n2);
-    var m = Math.pow(10, p2 - p1);
+  _div: (n1, n2) => {
+    let p1 = calcPos(n1),
+      p2 = calcPos(n2);
+    let m = Math.pow(10, p2 - p1);
     return FCU.mul(parseNum(n1) / parseNum(n2), m);
+  },
+  div: (...nums) => {
+    let result = nums[0];
+    for (let i = 1; i < nums.length; i++) result = FCU._div(result, nums[i]);
+    return result;
   }
 };
+
+export default FCU;
