c0e4f170d4d5e010381acf9ba4084667a7d286f1
CyberFei <im.gf@live.com>
2019-05-15 09:46:28 +0800

    bug修复，补充使用方法

--- index.js
+++ index.js
@@ -1,25 +1,27 @@
+// 获取小数点位数
 let calcPos = num =>
   num.toString().indexOf(".") > -1 ? num.toString().split(".")[1].length : 0;
+// 忽略小数点转为整数
 let parseNum = num => parseInt(num.toString().replace(".", ""));
 
-let FCU = {
+let FC = {
   // 加
   _add: (n1, n2) => {
     let p1 = calcPos(n1),
       p2 = calcPos(n2);
     let m = Math.pow(10, Math.max(p1, p2));
-    return (n1 * m + n2 * m) / m;
+    return (FC._mul(n1, m) + FC._mul(n2, m)) / m;
   },
   add: (...nums) => {
     let result = nums[0];
-    for (let i = 1; i < nums.length; i++) result = FCU._add(result, nums[i]);
+    for (let i = 1; i < nums.length; i++) result = FC._add(result, nums[i]);
     return result;
   },
   // 减
-  _sub: (n1, n2) => FCU.add(n1, -n2),
+  _sub: (n1, n2) => FC.add(n1, -n2),
   sub: (...nums) => {
     let result = nums[0];
-    for (let i = 1; i < nums.length; i++) result = FCU._sub(result, nums[i]);
+    for (let i = 1; i < nums.length; i++) result = FC._sub(result, nums[i]);
     return result;
   },
   // 乘
@@ -31,7 +33,7 @@ let FCU = {
   },
   mul: (...nums) => {
     let result = nums[0];
-    for (let i = 1; i < nums.length; i++) result = FCU._mul(result, nums[i]);
+    for (let i = 1; i < nums.length; i++) result = FC._mul(result, nums[i]);
     return result;
   },
   // 除
@@ -39,13 +41,13 @@ let FCU = {
     let p1 = calcPos(n1),
       p2 = calcPos(n2);
     let m = Math.pow(10, p2 - p1);
-    return FCU.mul(parseNum(n1) / parseNum(n2), m);
+    return FC.mul(parseNum(n1) / parseNum(n2), m);
   },
   div: (...nums) => {
     let result = nums[0];
-    for (let i = 1; i < nums.length; i++) result = FCU._div(result, nums[i]);
+    for (let i = 1; i < nums.length; i++) result = FC._div(result, nums[i]);
     return result;
   }
 };
 
-export default FCU;
+export default FC;
